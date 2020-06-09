d9f9b53dc5443f7dbf074ae50a00155f7b3b4890
rebarbe <srebarber2@huskers.unl.edu>
2020-06-09 18:36:25 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Sonja Rebarber
+ * Date: 2020/06/09
+ *
+ * A simply hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Sonja Rebarber\n");
+
+  return 0;
+}
